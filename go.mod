84e994a06dd1f26dd3e687c7cdf5f274ceb1f32b
0xd34d10cc <0xd34d10cc@gmail.com>
2019-11-05 18:48:39 +0000

    Re-implement service using swagger-go (#9)

--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module github.com/pl0q1n/No_More_Flex
 go 1.13
 
 require (
-	github.com/klauspost/compress v1.9.0 // indirect
-	github.com/valyala/fasthttp v1.5.0
+	github.com/go-openapi/errors v0.19.2
+	github.com/go-openapi/loads v0.19.4
+	github.com/go-openapi/runtime v0.19.7
+	github.com/go-openapi/spec v0.19.4
+	github.com/go-openapi/strfmt v0.19.3
+	github.com/go-openapi/swag v0.19.5
+	github.com/go-openapi/validate v0.19.4
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/rs/cors v1.7.0
+	golang.org/x/net v0.0.0-20191101175033-0deb6923b6d9
 )
---
+++ restapi/configure_nmf.go
@@ -0,0 +1,106 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	middleware "github.com/go-openapi/runtime/middleware"
+	swag "github.com/go-openapi/swag"
+	cors "github.com/rs/cors"
+
+	"github.com/pl0q1n/No_More_Flex/models"
+	"github.com/pl0q1n/No_More_Flex/restapi/operations"
+)
+
+var storage *Storage = NewStorage()
+
+//go:generate swagger generate server --target ../../nmf --name Nmf --spec ../swagger.yml
+
+func configureFlags(api *operations.NmfAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.NmfAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	api.AddTransactionHandler = operations.AddTransactionHandlerFunc(func(params operations.AddTransactionParams) middleware.Responder {
+		if err := storage.AddTransaction(UserID(0), *params.Body); err != nil {
+			return operations.NewAddTransactionDefault(500).WithPayload(&models.Error{Message: swag.String(err.Error())})
+		}
+		return operations.NewAddTransactionCreated()
+	})
+
+	api.FilterTransactionsHandler = operations.FilterTransactionsHandlerFunc(func(params operations.FilterTransactionsParams) middleware.Responder {
+		filter := NewTransactionsFilter()
+		if params.Category != nil {
+			filter.Category = *params.Category
+		}
+
+		if params.From != nil {
+			filter.From = *params.From
+		}
+
+		if params.To != nil {
+			filter.To = *params.To
+		}
+
+		if params.Sender != nil {
+			filter.Sender = *params.Sender
+		}
+
+		if params.Receiver != nil {
+			filter.Receiver = *params.Receiver
+		}
+
+		transactions, err := storage.FilterTransactions(UserID(0), filter)
+		if err != nil {
+			return operations.NewFilterTransactionsDefault(500).WithPayload(&models.Error{Message: swag.String(err.Error())})
+		}
+
+		return operations.NewFilterTransactionsOK().WithPayload(transactions)
+	})
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	handleCORS := cors.Default().Handler
+	return handleCORS(handler)
+}
